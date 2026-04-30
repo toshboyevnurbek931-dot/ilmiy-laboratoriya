efc10cb3a1e96306ef6c229e547b4a47adaaca1a
toshboyevnurbek931-dot <toshboyevnurbek931@gmail.com>
2026-04-30 10:53:47 +0000

    Lab2 va yangi menyu qo'shildi

--- app.py
+++ app.py
@@ -1,27 +1,33 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, abort
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
+    """Asosiy menyuni yuklash"""
     return render_template('menu.html')
 
-# 3-Laboratoriya: Faradey effekti
-@app.route('/lab3')
-def lab3():
-    return render_template('lab3.html')
-
-@app.route('/lab4')
-def lab4():
-    return render_template('index.html')
-
-@app.route('/lab5')
-def lab5():
-    return render_template('lab5.html')
-
-@app.route('/lab6')
-def lab6():
-    return render_template('lab6.html')
+@app.route('/lab<int:lab_id>')
+def lab(lab_id):
+    """
+    1 dan 30 gacha bo'lgan barcha laboratoriyalarni dinamik yuklash.
+    Masalan: /lab3 -> lab3.html ni yuklaydi.
+    """
+    # Laboratoriya raqami 1 va 30 oralig'ida ekanligini tekshiramiz
+    if 1 <= lab_id <= 30:
+        # Maxsus holatlar uchun (agar fayl nomi boshqacha bo'lsa)
+        if lab_id == 4:
+            return render_template('index.html')
+        
+        # Qolgan barcha laboratoriyalar uchun (lab1.html, lab2.html...)
+        try:
+            return render_template(f'lab{lab_id}.html')
+        except:
+            # Agar fayl hali yaratilmagan bo'lsa, xatolik bermaslik uchun
+            return f"<h1>{lab_id}-Laboratoriya ishi tayyorlanmoqda...</h1><p>Tez kunda yuklanadi.</p>"
+    else:
+        # Agar 30 dan katta raqam kiritilsa 404 xatosi
+        abort(404)
 
 if __name__ == '__main__':
     app.run(debug=True)
